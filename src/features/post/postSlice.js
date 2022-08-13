3009bdb2d13e447bba01b76ecf3ed3324dfce345
Smarsden007 <seannmarsden@gmail.com>
2022-08-13 12:50:50 -0700

    postList is built out, will only grab first 100 chars as preview

--- src/features/post/postSlice.js
+++ src/features/post/postSlice.js
@@ -1,30 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = [
-    {
-        id: '1',
-        title: 'Learning Redux Toolkit',
-        content: "I've heard good things.",
-        date: sub(new Date(), { minutes: 10 }).toISOString(),
-        reactions: {
-            thumbsUp: 0,
-            wow: 0,
-            heart: 0,
-            rocket: 0,
-            coffee: 0
-        }
-    },
-    {
-        id: '2',
-        title: 'Slices...',
-        content: "The more I say slice, the more I want pizza.",
-        date: sub(new Date(), { minutes: 5 }).toISOString(),
-        reactions: {
-            thumbsUp: 0,
-            wow: 0,
-            heart: 0,
-            rocket: 0,
-            coffee: 0
-        }
-    }
+   { id: '1', title: 'Learning Reux Toolkit',content: "I've heard good things"},
+   { id: '2', title: 'Learning Reux Toolkit2',content: "I've heard good things2"}
 ]
+
+const postSlice = createSlice({
+    name: 'posts',
+    initialState,
+    reducers: {}
+});
+
+export default postSlice.reducer
