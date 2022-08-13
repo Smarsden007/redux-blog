e2ef3d4c35055b95d85a3fe86b20bf364e1a9a0c
Smarsden007 <seannmarsden@gmail.com>
2022-08-13 12:01:02 -0700

    building counterSlice out to handle blog actions

--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -10,6 +10,13 @@ export const counterSlice = createSlice({
     reducers: {
         increment: (state) => {
             state.count += 1;
+        },
+        decrement: (state) => {
+            state.count -= 1;
         }
     }
-})
+});
+
+export const { increment, decrement } = counterSlice.actions;
+
+export default counterSlice.reducer;
