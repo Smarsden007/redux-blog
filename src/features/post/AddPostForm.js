188cf9ea8b416279c6aaa7db26c15cfd1f0ddc25
Smarsden007 <seannmarsden@gmail.com>
2022-08-13 20:34:58 -0700

    havng conflict with map value on form

--- src/features/post/AddPostForm.js
+++ src/features/post/AddPostForm.js
@@ -1,27 +1,49 @@
 import { useState } from "react";
-import React from 'react'
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
-import { postAdded } from "./postsSlice";
+import { addNewPost } from "./postsSlice";
+import { selectAllUsers } from "../users/usersSlice";
 
 const AddPostForm = () => {
     const dispatch = useDispatch()
+
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
+    const [userId, setUserId] = useState('')
+    const [addRequestStatus, setAddRequestStatus] = useState('idle')
 
+    const users = useSelector(selectAllUsers)
 
     const onTitleChanged = e => setTitle(e.target.value)
     const onContentChanged = e => setContent(e.target.value)
-    
+    const onAuthorChanged = e => setUserId(e.target.value)
+
+
+    const canSave = [title, content, userId].every(Boolean) && addRequestStatus === 'idle';
+
     const onSavePostClicked = () => {
-        if (title && content) {
-            dispatch(
-                postAdded(title,content)
-            )
-            setTitle('')
-            setContent('')
+        if (canSave) {
+            try {
+                setAddRequestStatus('pending')
+                dispatch(addNewPost({ title, body: content, userId })).unwrap()
+
+                setTitle('')
+                setContent('')
+                setUserId('')
+            } catch (err) {
+                console.error('Failed to save the post', err)
+            } finally {
+                setAddRequestStatus('idle')
+            }
         }
+
     }
+
+    const usersOptions = users.map(user => (
+        <option key={user.id} value={user.id}>
+            {user.name}
+        </option>
+    ))
 
     return (
         <section>
@@ -35,8 +57,11 @@ const AddPostForm = () => {
                     value={title}
                     onChange={onTitleChanged}
                 />
-
-
+                <label htmlFor="postAuthor">Author:</label>
+                <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
+                    <option value=""></option>
+                    {usersOptions}
+                </select>
                 <label htmlFor="postContent">Content:</label>
                 <textarea
                     id="postContent"
@@ -46,7 +71,9 @@ const AddPostForm = () => {
                 />
                 <button
                     type="button"
-                    onClick={onSavePostClicked}>Save Post</button>
+                    onClick={onSavePostClicked}
+                    disabled={!canSave}
+                >Save Post</button>
             </form>
         </section>
     )
