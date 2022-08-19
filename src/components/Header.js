ea5accf3ec4cad530198549355e66fa98c4b5ebc
Smarsden007 <seannmarsden@gmail.com>
2022-08-19 00:04:27 -0700

    imported logo and working on header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,29 @@
 import { Link } from "react-router-dom"
+import logo from './../media/letterway.png'
+import styled from "styled-components"
 
 
 const Header = () => {
+    const HeaderUl = styled.ul`
+    padding: 7rem;
+    color: white;
+    `
+    const HeaderLi= styled.li`
+    font-family: monospace;
+    font-size: 2rem;
     
+    `
     return (
         <header className="Header">
-            <h1>Redux Blog</h1>
+            <h1><img alt="letterwatlogo" src={logo} width={250} ></img></h1>
             <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="post">Post</Link></li>
-                    <li><Link to="user">Users</Link></li>
-                </ul>
-                
+
+                <HeaderUl>
+                    <HeaderLi><Link to="/">Home</Link></HeaderLi>
+                    <HeaderLi><Link to="post">Post</Link></HeaderLi>
+                    <HeaderLi><Link to="user">Users</Link></HeaderLi>
+                </HeaderUl>
+
             </nav>
         </header>
     )
